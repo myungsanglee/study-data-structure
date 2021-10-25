49bc23f42aaf544f4664994ee9b6d550c8883be3
myungsanglee <lee0577@naver.com>
2021-10-25 22:18:45 +0900

    update 'IntStack Pop func'

--- chapter 05/train/train_5_5/train_5_5.c
+++ chapter 05/train/train_5_5/train_5_5.c
@@ -11,12 +11,9 @@ Top:
     if (n > 0) {
         Push(&stack, n);
         n = n - 1;
-        Print(&stack);
         goto Top;
     }
-    Print(&stack);
     if (!IsEmpty(&stack)) {
-        Print(&stack);
         Pop(&stack, &n);
         printf("%d\n", n);
         n = n - 2;
